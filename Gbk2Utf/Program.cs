7d85d26834e0ea6e260437ee0073c86db54ca353
Gaein nidb <mail@gaein.cn>
2021-04-22 18:37:29 +0800

    Update

--- Gbk2Utf/Program.cs
+++ Gbk2Utf/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
-using System.Data;
 using System.IO;
 using System.Text;
 
-// Get file info.
-Console.WriteLine("Please input a file name. The file and this program must in a same dir.");
-var filename = Console.ReadLine() ?? throw new NoNullAllowedException();
-var filePath = Path.Combine(Environment.CurrentDirectory, filename);
+#region Get file info.
+Console.WriteLine("== CONVERT GB2312 ENCODING TO UTF8 ==");
+
+Console.WriteLine("Please input a path that storage the data file.");
+var basePath = Console.ReadLine() ?? Environment.CurrentDirectory;
+
+Console.WriteLine("Please input the name of the file.");
+var fileName = Console.ReadLine() ?? throw new Exception("Error input value.");
+var filePath = Path.Combine(basePath, fileName);
+
+if (!File.Exists(filePath)) throw new FileNotFoundException($"Can not found file:{filePath}");
 var fileInfo = new FileInfo(filePath);
+#endregion
 
-// Read file content.
+#region Read file content.
 Console.WriteLine("Start read file{0}", fileInfo.FullName);
-var content = await File.ReadAllTextAsync(filename, Encoding.GetEncoding("GBK"));
+var content = await File.ReadAllTextAsync(filePath, Encoding.GetEncoding("GB2312"));
 var newFilePath = Path.Combine(Environment.CurrentDirectory, fileInfo.Name + "-utf8" + fileInfo.Extension);
+#endregion
 
-// Write file in UTF-8.
-Console.WriteLine("Output UTF-8 encoding content to file{0}");
+#region Write file in UTF-8.
+Console.WriteLine("Output UTF-8 encoding content to file{0}", filePath);
 await File.WriteAllTextAsync(newFilePath, content, Encoding.UTF8);
+#endregion
